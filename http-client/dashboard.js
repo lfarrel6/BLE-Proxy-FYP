c4fb21c006fb05b7bc32718bda386466477a7318
lfarrel6 <lfarrel6@tcd.ie>
2018-03-03 14:42:31 +0000

    List displays data from server which will be coap response

--- http-client/dashboard.js
+++ http-client/dashboard.js
@@ -1,29 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var coap = require('coap');
+var path = require('path');
 
 var ipAddr;
 
 router.get('/:ip', function(req,res){
-	//res.send('GET request to ' + req.params.ip);
+	res.sendFile(path.join(__dirname, '/public/pages/dash.html'));
+});
 
+router.get('/:ip/observations',function(req,res){
 	ipAddr = req.params.ip;
 
 	var coapReq = coap.request('coap://'+ipAddr+':5683/.well-known/core');
-	console.log(coapReq);
+	//console.log(coapReq);
 	var responseValue;
+
+	for(var i = 0; i < 25; i++){
+		responseValue+=''+i+'\n';
+		console.log(i);
+	}
+
 	coapReq.on('response',function(coapRes){
+
 		coapRes.on('data',function(chunk){
 			console.log(chunk);
-			responseValue+=chunk + ' ';
+			//responseValue+=chunk + ' ';
+			//res.append(chunk);
 		});
+		
 		coapRes.on('end', function(){
-			console.log('message complete');
-			res.send(responseValue);
+			console.log('message complete: ', responseValue);
+			console.log('Response:', res);
+			res.end();
 		});
+
 	});
 	coapReq.end();
 	console.log('request finished\n'+responseValue);
+	res.send(responseValue);
 });
 
 module.exports = router;
