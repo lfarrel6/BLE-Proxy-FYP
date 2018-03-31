20d5a38d6478864c0964f0e6ee77c85fd1f29c05
Liam <lfarrel6@tcd.ie>
2018-03-31 16:32:57 +0100

    Promises implemented to allow for timeouts

--- Centrals/Explorer/ble-explorer.js
+++ Centrals/Explorer/ble-explorer.js
@@ -266,7 +266,7 @@ function getServicesSync(index,response){
 	var requestedPeripheral = discoveries[index].peripheral;
 	var url_paths={};
 
-	var getServices = new Promise(function(resolve,reject){
+	var retrieveServicesSync = new Promise(function(resolve,reject){
 		console.log('Synchronous service retrieval');
 
 		requestedPeripheral.on('disconnect',function(){
@@ -305,7 +305,7 @@ function getServicesSync(index,response){
 		}, 30000);
 	});
 	
-	getServices.then(function(result){
+	retrieveServicesSync.then(function(result){
 
 		requestedPeripheral.disconnect();
 		response.write(result);
@@ -315,7 +315,7 @@ function getServicesSync(index,response){
 	}).catch(function(err){
 		
 		requestedPeripheral.disconnect();
-		response.write(err);
+		response.write('Error: ' + err);
 		response.end();
 		noble.startScanning();
 
@@ -324,234 +324,269 @@ function getServicesSync(index,response){
 }
 
 //response optional parameter
-function getServices(index, callback){
+function getServices(index,response){
 	noble.stopScanning();
 	var requestedPeripheral = discoveries[index].peripheral;
 	console.log(chalk.cyan('Fetching Services'));
 	var url_paths = {};
 
-	requestedPeripheral.on('disconnect', function(){
-		console.log(chalk.green('> Requested Peripheral disconnect @ ' + new Date()));
-		noble.startScanning();
-	});
+	var serviceRetrieval = new Promise(function(resolve,reject){
+		console.log("Async service retrieval");
 
-	requestedPeripheral.connect(function(err){
-
-		console.log(chalk.cyan('Connecting to peripheral'));
-
-		if(err){
-			console.log(chalk.red('CONNECT ERROR: ' + err));
-		}
-
-		requestedPeripheral.discoverServices([], function(error, services){
-			var i = 0, paths = 0;
-			console.log(chalk.cyan('Service discovery'));
-			async.whilst(
-				function(){ return i < services.length; },
-				function(serviceCallback){
-					var service = services[i];
-
-			        //if (services_lookup_table[service.uuid]) {
-			        	url_paths[service.uuid] = getCharacteristics(requestedPeripheral,service.uuid);
-			        	paths++;
-
-			        	console.log(chalk.green('> ' + service.uuid));
-			        //}
-			        i++;
-			        serviceCallback(null, url_paths);
-				},
-				function(err, results){ 
-					if(err){
-						callback(JSON.stringify(err));
-
-						requestedPeripheral.disconnect();
-						discoveries[index].paths = [];
-						
-						console.log(chalk.red('L239, ASYNC ERROR: ' + err));
-					}else{
-						callback(JSON.stringify(results));
-						requestedPeripheral.disconnect();
-						discoveries[index].paths = results;
-					}
-				}
-			);
+		requestedPeripheral.on('disconnect', function(){
+			console.log('Disconnected in service retrieval');
 		});
-	});
-}
 
-function getCharacteristics(peripheral, uuid, callback){
-	noble.stopScanning();
-	console.log(chalk.bgCyan('Getting ' + uuid + ' from ' + peripheral.advertisement.localName));
+		requestedPeripheral.connect(function(err){
 
-	peripheral.on('disconnect',function(){
-		console.log('Disconnected in getCharacteristics');
-		noble.startScanning();
-	});
+			console.log(chalk.cyan('Connecting to peripheral'));
+			if(err){
+				console.log(chalk.red('Connect Error: ' + err));
+				reject(err);
+			}
+			else{
+				requestedPeripheral.discoverServices([], function(error,services){
 
-
-	peripheral.connect(function(connectErr){
-		console.log(chalk.bgCyan('Connection formed'));
-		if(connectErr){
-			console.log(chalk.red('ERROR GETTING CHARACTERISTICS: ' + connectErr));
-			callback(connectErr);
-		}else{
-			console.log(chalk.bgCyan('No error before discoverServices'));
-			peripheral.discoverServices([uuid], function(servicesErr,services){
-
-				if(servicesErr){
-					console.log(chalk.red('ERROR DISCOVERING SERVICE (' + uuid + '): ' + servicesErr));
-					callback(servicesErr);
-				}else{
-					console.log('services length @ 393: '+services.length);
-					for(var i = 0; i < services.length; i++){
-						var service = services[i];
-						service.discoverCharacteristics([], function(characteristicsErr, characteristics){
-							if(characteristicsErr){
-								console.log(chalk.red('ERROR DISCOVERING CHARACTERISTICS: ' + characteristicsErr));
-								callback(characteristicsErr);
-							}else{
-								console.log(characteristics);
-								var result = {};
-								for(var j = 0; j < characteristics.length; j++){
-
-									console.log(chalk.bgCyan(j + ': being added to result'));
-									var thisChar = characteristics[j];
-									result[thisChar.uuid] = thisChar.name;
-
-								}
-								peripheral.disconnect();
-								callback(JSON.stringify(result));
-							}
-						})
-					}
-				}
-
-			});
-
-		}
-	});
-	
-}
-
-/*
-peripheral: noble peripheral object with which connection is being sought
-uuid: desired services uuid
-*/
-function exploreService(peripheral, uuid){
-	var info = [];
-	peripheral.discoverServices([uuid], function(error, services){
-		
-		var i = 0;
-
-		async.whilst(
-			function(){ return i < services.length; },
-			function(callback){
-				var service = services[i];
-
-				service.discoverCharacteristics([], function(error, characteristics){
-
-					var j = 0;
+					var i = 0, paths = 0;
 
 					async.whilst(
-						function(){ return j < characteristics.length; },
-						function(nested_callback){
-							var characteristic = characteristics[j];
+						function(){ return i < services.length; },
+						function(serviceCallback){
+							var service = services[i];
 
-							nested_callback(null, info.push(characteristic));
-							j++;
+							if(!url_paths[service.uuid]){
+								url_paths[service.uuid] = {};
+							}
+							console.log(chalk.green('> ' + service.uuid));
+							i++;
+
+							serviceCallback(null,url_paths);
 						},
 						function(err,results){
 							if(err){
-								callback(err);
+								reject(err);
 							}else{
-								callback(null,results);
+								if(Object.keys(url_paths).length > Object.keys(discoveries[index].paths).length){
+									discoveries[index].paths = url_paths;
+								}
+								resolve(JSON.stringify(discoveries[index].paths));
 							}
 						}
-					);
+					)
 
 				});
-				i++;
-			},
-			function(err,results){
-				if(err){
-					console.log(chalk.red('L296, ASYNC ERROR: ' + err));
-					return null;
-				}else{
-					return results;
-				}
 			}
-		);
+		});
+
+		setTimeout(reject('Timeout'), 60000); //timeout after 1 minute
+
 	});
+
+	serviceRetrieval.then(function(results){
+		
+		requestedPeripheral.disconnect();
+		response.write(results);
+		response.end();
+		noble.startScanning();
+
+	}).catch(function(error){
+		
+		requestedPeripheral.disconnect();
+		response.write('Error: ' + error);
+		response.end();
+		noble.startScanning();
+
+	});
+
+}
+
+function getCharacteristics(peripheral, uuid, response){
+	noble.stopScanning();
+	console.log(chalk.bgCyan('Getting ' + uuid + ' from ' + peripheral.advertisement.localName));
+
+
+	var charsRetrieval = new Promise(function(resolve,reject){
+
+		peripheral.on('disconnect', function(){
+			console.log('Disconnect in getCharacteristics');
+		});
+
+		peripheral.connect(function(connectErr){
+
+			if(connectErr){
+				console.log(chalk.red('Error in get characteristics: ' + connectErr));
+				reject(connectErr);
+			}else{
+				console.log(chalk.bgCyan('Connected in get characteristics'));
+				peripheral.discoverServices([uuid], function(servicesErr,services){
+
+					if(servicesErr){
+						console.log(chalk.red('Error discovering services: ' + servicesErr));
+						reject(servicesErr);
+					}else{
+						console.log('Services length: ' + services.length);
+
+						for(var i = 0; i < services.length; i++){
+
+							var service = services[i];
+							service.discoverCharacteristics([],function(characteristicsErr,characteristics){
+								if(characteristicsErr){
+									
+									console.log(chalk.red('Error discovering characteristics: ' + characteristicsErr));
+									reject(characteristicsErr);
+
+								}else{
+
+									console.log(chalk.bgCyan(characteristics));
+									var results = {};
+									for(var j = 0; j < characteristics.length; j++){
+										var thisChar = characteristics[j];
+										results[thisChar.uuid] = thisChar.name;
+									}
+
+									resolve(JSON.stringify(results));
+
+								}
+							})
+
+						}
+					}
+
+				});
+			}
+		})
+
+		setTimeout(reject('Timeout'), 45000);
+	});
+
+	charsRetrieval.then(function(results){
+		peripheral.disconnect();
+		response.write(results);
+		response.end();
+		noble.startScanning();
+	}).catch(function(error){
+
+		peripheral.disconnect();
+		response.write('Error: ' + results);
+		response.end();
+		noble.startScanning();
+
+	});
+	
 }
 
 /*
 read given characteristic
 */
 function read(response, deviceJSON, service, characteristic){
+	noble.stopScanning();
+	console.log('Reading characteristic ' + characteristic);
+
 	var peripheral = deviceJSON.peripheral;
 
-	peripheral.connect(function(conn_error){
-		peripheral.discoverServices([service],function(service_err, services){
+	var readChar = new Promise(function(resolve,reject){
 
-			if(service_err){
-				console.log(chalk.red('Error in connecting to service (' + services_lookup_table[service] + '): ' + service_err));
-			}else{
-				var i = 0;
-				async.whilst( 
-					function(){ return i < services.length; }, 
-					function(callback){
-						var service = services[i];
-
-						service.discoverCharacteristics([characteristic], function(character_err,characteristics){
-				
-							if(character_err){
-								console.log(chalk.red('L328, Error connecting to characteristic (', chars_lut[characteristic] + '): ' + character_err));
-								callback(character_err);
-							}else{
-								var response_data;
-				
-								var j = 0;
-
-								async.whilst(
-									function(){ return j < characteristics.length; },
-									function(nested_callback){
-										var c = characteristics[j];
-
-										c.read(function(err,data){
-											if(err){
-												console.log(chalk.red('L342, Error Reading Characteristic (' + chars_lut[c] + '): ' + err));
-											
-												nested_callback(err);
-											}else{
-												response_data+=data;
-												nested_callback(null,response_data);
-											}
-										});
-										j++;
-									},
-									function(err, results){
-										if(err){
-											console.log(chalk.red('L355, Error during Characteristic read: ', err));
-											callback(err);
-										}else{
-											callback(null,results);
-										}
-									});
-							}
-							i++;
-						})
-					},
-					function(err,results){
-						if(err){
-							console.log(chalk.red('L367, ASYNC ERROR: ' + err));
-						}else{
-							response.json(results);
-							//requestComplete(response,0);
-						}
-					});
-			}
+		peripheral.on('disconnect',function(){
+			console.log('Disonnect in read');
 		});
+
+		peripheral.connect(function(connErr){
+
+			if(connErr){
+			
+				console.log('Connection eror in read: ' + connErr);
+				reject(connErr);
+			
+			}else{
+
+				peripheral.discoverServices([service],function(serviceErr,services){
+
+					if(serviceErr){
+						console.log('Service discovery error: ' + serviceErr);
+						reject(serviceErr);
+					}else{
+						var i = 0;
+						async.whilst(
+							function(){ return i < services.length; },
+							function(callback){
+								var service = services[i];
+								i++;
+								service.discoverCharacteristics([characteristic], function(charErr,characteristics){
+
+									if(charErr){
+										console.log(chalk.red('Characteristic discovery error: ' + charErr));
+										reject(charEr);
+									}else{
+
+										var responseData, j = 0;
+
+										async.whilst(
+											function(){ return j < characteristics.length; },
+											function(nested_callback){
+												var c = characteristics[j];
+
+												c.read(function(err,data){
+													if(err){
+														console.log(chalk.red('Error in read: ' + err));
+														reject(err);
+													}else{
+
+														responseData+=data;
+														j++;
+														nested_callback(null,responseData);
+
+													}
+												});
+											},
+											function(error,result){
+												if(error){
+													reject(error);
+												}else{
+													resolve(JSON.stringify(result));
+												}
+											}
+										);
+
+									}
+
+								});
+							},
+							function(error,results){
+								if(error){
+									reject(error);
+								}else{
+									resolve(JSON.stringify(results));
+								}
+							}
+						);
+					}
+
+				});
+
+			}
+
+		});
+
+		setTimeout(reject('Timeout'),90000); // timeout after a minute and a half
+
 	});
+
+	readChar.then(function(result){
+		
+		peripheral.disconnect();
+		response.write(result);
+		response.end();
+		noble.startScanning();
+
+	}).catch(function(error){
+
+		peripheral.disconnect();
+		response.write(error);
+		response.end();
+		noble.startScanning();
+
+	});
+
 }
 /*
 setInterval(function(){
